5c7ce14631075a67053c0068e6fec485f3686b53
Mrcnu <134508260+Mrcnu@users.noreply.github.com>
2026-09-04 13:02:22 +0530

    Update info.py

--- info.py
+++ info.py
@@ -88,9 +88,9 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 
 
 # Links
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/Movie_ReSearch')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/CnuNetwork')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/CnuTeam')
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'Movie_ReSearch') # Support Chat Link Without https:// or @
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'CnuNetwork') # Support Chat Link Without https:// or @
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/Keerthyadminbot')
 
 # True Or False
